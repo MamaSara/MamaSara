7a31dff276a03d932f7b88d92a23dec6f56241ab
HoomanTahmasebipour <hooman.tahmasebipour@mail.utoronto.ca>
2021-04-16 19:47:36 -0400

    setup and instalation script finalized. Passing all tests on fresh OS

--- automation/.bashrc
+++ automation/.bashrc
@@ -115,7 +115,6 @@ fi
 ##########################################################################
 # COPY THE BELOW LINES INTO /home/pi/.bashrc file on your own device #
 
-source /home/pi/MamaSara_venv/bin/activate
 cd /home/pi/MamaSara
 python3 /home/pi/MamaSara/src/MamaSara.py
 
--- setup/install_and_setup.sh
+++ setup/install_and_setup.sh
@@ -1,8 +1,10 @@
-printf "Thank you for using this script"
-printf "This script is stored at: https://github.com/MamaSara/MamaSara.git"
-printf "This script assumes the MamaSara repository is stored in the Pi home directory, with path: ~/MamaSara/"
-printf "This script will automatically be run in the setup directory of this repo, and will create the MamaSara_venv directory here"
-printf "The installation is starting......\n\n\n"
+printf "Thank you for using this script\n"
+printf "This script is stored at: https://github.com/MamaSara/MamaSara.git\n"
+printf "This script assumes the MamaSara repository is stored in the Pi home directory, with path: /home/pi/MamaSara/\n"
+printf "This script will automatically be run in the setup/bin directory of this repository\n"
+printf "The contents of /home/pi/MamaSara/setup/bin can be deleted when the installation is complete.\n"
+printf "The installation is starting ......\n\n\n"
+cd /home/pi/MamaSara/setup/bin
 
 printf "#####Update and upgrade Raspberry Pi#####\n\n"
 sudo apt-get update
@@ -17,7 +19,7 @@ sudo apt-get install -y libbz2-dev libssl-dev build-essential tk-dev libncurses5
  libgfortran5 libatlas3-base libatlas-base-dev libopenblas-dev libopenblas-base libblas-dev liblapack-dev\
  cython libatlas-base-dev openmpi-bin libopenmpi-dev python3-dev python3-pip wget git curl python-rpi.gpio\
  python3-rpi.gpio espeak
-print "\n\n\n"
+printf "\n\n\n"
 
 printf "#####Install python3.7.8#####\n\n"
 wget https://www.python.org/ftp/python/3.7.8/Python-3.7.8.tgz
@@ -32,10 +34,6 @@ printf "\n\n\n"
 printf "#####Upgrade pip and setuptools#####\n\n"
 pip install --upgrade pip
 pip install --upgrade setuptools
-printf "\n\n\n"
-
-printf "##### Put all executables in /home/pi/MamaSara/setup/bin #####"
-cd /home/pi/MamaSara/setup/bin
 printf "\n\n\n"
 
 printf "#####Install Docker and Docker-Compose#####\n\n"
@@ -53,18 +51,18 @@ printf "#####Install additional dependancies with pip#####\n\n"
 sudo pip3 install scipy tensorflow pandas pyttsx3 SpeechRecognition pyaudio numpy pyyaml requests RPi.GPIO gpiozero board Adafruit-Blinka adafruit-circuitpython-charlcd
 printf "\n\n\n"
 
-printf "##### Pulling Rasa Docker Image from DockerHub #####"
+printf "##### Pulling Rasa Docker Image from DockerHub #####\n\n"
 sudo docker pull hoomant/mamasara:mamasara_rasa
 printf "\n\n\n"
 
-printf "##### Pulling DeepSpeech Docker Image from DockerHub #####"
+printf "##### Pulling DeepSpeech Docker Image from DockerHub #####\n\n"
 sudo docker pull cwrogers1/mamasara-deepspeech:micvad
 printf "\n\n\n"
 
 cd ~
-printf "\n\n------------------------------------------------------------"
-printf "Congratulations! The MamaSara environment is now installed and ready for use on your Raspberry Pi"
-printf "To test it out the MamaSara application:\n\t1. source ~/MamaSara_env/bin/activate\n\t2.python3 /home/pi/MamaSara/src/MamaSara.py"
-printf "To test out Rasa with its interactive Shell:\n\t1. source ~/MamaSara_env/bin/activate\n\t2. sudo docker-compose run rasa run actions\n\t3. sudo docker-compose run rasa shell"
-printf "IMPORTANT: To enable non-root access to docker and running containers, perform the following steps:\n\t1. sudo usermod -aG docker [user_name] (ex: sudo usermod -aG docker pi)\n\t2. Log out of user account and log back in for this to take effect.\n\nFor more information, refer to: https://phoenixnap.com/kb/docker-on-raspberry-pi"
-printf "Enjoy MamaSara!"
+printf "\n\n------------------------------------------------------------\n\n"
+printf "Congratulations! The MamaSara environment is now installed and ready for use on your Raspberry Pi\n"
+printf "To test it out the MamaSara application:\n\t1. source ~/MamaSara_env/bin/activate\n\t2. python3 /home/pi/MamaSara/src/MamaSara.py\n"
+printf "To test out Rasa with its interactive Shell:\n\t1. source ~/MamaSara_env/bin/activate\n\t2. sudo docker-compose run rasa run actions\n\t3. sudo docker-compose run rasa shell\n"
+printf "IMPORTANT: To enable non-root access to docker and running containers, perform the following steps:\n\t1. sudo usermod -aG docker [user_name] (ex: sudo usermod -aG docker pi)\n\t2. Log out of user account and log back in for this to take effect.\nFor more information, refer to: https://phoenixnap.com/kb/docker-on-raspberry-pi\n\n"
+printf "Enjoy MamaSara!\n"
